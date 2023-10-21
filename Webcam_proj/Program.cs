a92c1bcdf118fee1543ab2d0d6fe4bc96089adf3
artursab <arturs.belavskis@gmail.com>
2023-10-21 17:26:07 +0200

    bitmap

--- Webcam_proj/Program.cs
+++ Webcam_proj/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
 using AForge.Video.DirectShow;
 
 
@@ -13,6 +18,7 @@ namespace Webcam_proj
     class Program
     {
         private static IPEndPoint consumerEndPoint;
+        private static UdpClient udpClient = new UdpClient();
         
         static void Main(string[] args)
         {
@@ -24,6 +30,29 @@ namespace Webcam_proj
 
             FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
             VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            var bmp = new Bitmap(eventArgs.Frame, 800, 600);
+
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    bmp.Save(ms, ImageFormat.Jpeg);
+                    var bytes = ms.ToArray();
+                    udpClient.Send(bytes, bytes.Length, consumerEndPoint);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
