42f385bc8d6b7cd310ee8d2c7ad855e6618c32a3
artursab <arturs.belavskis@gmail.com>
2023-10-21 17:12:34 +0200

    aforge

--- Webcam_proj/Program.cs
+++ Webcam_proj/Program.cs
@@ -1,22 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
+
 
 namespace Webcam_proj
 {
-    internal static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        private static IPEndPoint consumerEndPoint;
+        
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var consumerIp = ConfigurationManager.AppSettings.Get("consumerIp");
+            var consumerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consumerIp), consumerPort);
+
+            Console.WriteLine($"consumer: {consumerEndPoint}");
+
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
         }
     }
 }
